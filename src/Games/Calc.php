eec0b4dd243c51297799246d3a6d043cf391e8a2
Greentus <very.green@gmail.com>
2021-01-29 15:26:50 +0200

    Final step

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -14,7 +14,7 @@ function calc()
         $num1 = rand(1, MAX_NUMBER);
         $num2 = rand(1, MAX_NUMBER);
         $op = rand(0, count($opertions) - 1);
-        $expr = $num1 . $opertions[$op] . $num2;
+        $expr = $num1 . ' ' . $opertions[$op] . ' ' . $num2;
         eval("\$sum={$expr};");
         $expressions[] = [$expr,$sum];
     }
